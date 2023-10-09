fd5a4f8c2cfed8d915b9ba3e168d266f09c36291
streethagore <srivaud972@gmail.com>
2023-10-10 01:47:19 +0200

    Implementing delayed backpropagation

---
+++ slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=async
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=04:30:00
+#SBATCH --output=slurm/%x-%j.out
+#SBATCH --error=slurm/%x-%j.err
+
+# Load the conda module
+source /usr/local/miniconda/etc/profile.d/conda.sh
+conda activate async
+
+python -u main.py --delay $1 | tee "delay_${1}.log"
