66fe0b685c39cf30b7d97412f6acedb5f01f3642
streethagore <srivaud972@gmail.com>
2023-10-13 01:12:24 +0200

    Fixing code

--- launch_slurm.sh
+++ launch_slurm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for delay in 1 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34; do
+for delay in 0 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35; do
   sbatch launch_main_script.sh $delay
 done
