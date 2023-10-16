85180a9ec57f46fe6d72ecd74f13ce8b1b2bfccd
Stephane Rivaud <stephane.rivaud@isir.upmc.fr>
2023-10-16 13:48:52 +0200

    Using affinve convolution even if followed by affine batch normalization

--- models/resnet.py
+++ models/resnet.py
@@ -17,17 +17,17 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+                               stride=1, padding=1, bias=True)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
-                          kernel_size=1, stride=stride, bias=False),
+                          kernel_size=1, stride=stride, bias=True),
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
@@ -76,7 +76,7 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+                               stride=1, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -99,6 +99,7 @@ class ResNet(nn.Module):
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
+        print(out.size())
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
