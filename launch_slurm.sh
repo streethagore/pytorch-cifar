d9fcf1e242279bd488a80441f188a7e394d30b12
Stephane Rivaud <stephane.rivaud@isir.upmc.fr>
2023-10-17 12:01:54 +0200

    Modifying scripts for batch of experiments

--- launch_slurm.sh
+++ launch_slurm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-for delay in 0 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35; do
+#for delay in 0 1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 31 33 35; do
+for delay in {2..32..2}; do
   sbatch launch_main_script.sh $delay
 done
