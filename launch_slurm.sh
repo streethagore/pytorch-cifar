b4db4049033f9b9468f93edf9ad25cf74b0b038d
Stephane Rivaud <stephane.rivaud@isir.upmc.fr>
2023-10-18 16:21:34 +0200

    Testing custom weight decay

--- launch_slurm.sh
+++ launch_slurm.sh
@@ -5,12 +5,14 @@ partition=$1
 for delay in {0..1..1}; do
   decay_mode='pytorch'
   decay_delayed='false'
+  echo $delay $decay_mode $decay_delayed
   sbatch -p $partition launch_main_script.sh $delay $decay_mode $decay_delayed
 done
 
 for delay in {0..1..1}; do
   for decay_mode in 'loss' 'weights'; do
     for decay_delayed in 'true' 'false'; do
+      echo $delay $decay_mode $decay_delayed
       sbatch -p $partition launch_main_script.sh $delay $decay_mode $decay_delayed
     done
   done
